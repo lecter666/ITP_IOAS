ed3cf2d55fc582745ae9dc23d8338a7c48f77648
Akilan arul <ahilanqwertyuiop@gmail.com>
2015-08-23 10:32:55 +0530

    validation and hashing

---
+++ ITP_OFFICE_ASSISTANCE_SYSTEM/ITP_OFFICE_ASSISTANCE_SYSTEM/String_satatization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ITP_OFFICE_ASSISTANCE_SYSTEM
+{
+    class String_satatization
+    {
+        //by MSDN http://msdn.microsoft.com/en-us/library/844skk0h(v=vs.71).aspx
+        public static string sanitize (string strIn)
+        {
+            // Replace non alphanumeric characters with empty strings.
+            return Regex.Replace(strIn, @"[^\w\.]", "");
+        }
+        
+        public bool analyze(string strIn)
+        {
+            // Check if there are any non word characters.
+            return Regex.IsMatch(strIn, @"[^\?/;:`~!@#$%^&*()_+\-=\\'""']");
+        }
+
+        //public void 
+
+    }
+}
