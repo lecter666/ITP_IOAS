be9ee81f44a2b167a577995a1718567ef0a1a625
Akilan arul <ahilanqwertyuiop@gmail.com>
2015-08-23 16:20:13 +0530

    dbconn

---
+++ ITP_OFFICE_ASSISTANCE_SYSTEM/ITP_OFFICE_ASSISTANCE_SYSTEM/DBconn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITP_OFFICE_ASSISTANCE_SYSTEM
+{
+    class DBconn
+    {
+        public void connect()
+        {
+            SqlConnection conn = new SqlConnection("Server=AKILAN-PC;Database=test;Trusted_Connection=yes;");
+            conn.Open();
+
+        }
+    }
+}
